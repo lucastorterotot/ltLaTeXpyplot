8b6d142dc06e706ddd631b0120fbe5b2cca3c3e1
lucastorterotot <lucas.torterotot@gmail.com>
2019-01-31 11:35:27 +0100

    Cu EpH data update.

--- data/EpH/Cu.py
+++ data/EpH/Cu.py
@@ -1,50 +1,74 @@
 from EpHgeneric import *
 
-
 Eza = 0.52
 Eab = 0.16
-pKsCuOHOH = 20
-pKsCuCuO = 15
+pKs_Cu_2OH = 18
+pKs_Cu_Cu2O = 14
 
 def pHsepa(pC):
-    return 14-(pKsCuCuO-pC)/1.
+    return 14-(pKs_Cu_Cu2O-pC)/1.
 
 def pHsepb(pC):
-    return 14-(pKsCuOHOH-pC)/2.
+    return 14-(pKs_Cu_2OH-pC)/2.
 
-def Esepza(pC, pH):
+def pHcrois(pC):
+    return pHsepa(pC) + .5*(Esepza1(pC, pHsepa(pC))-Esepab1(pC, pHsepa(pC)))/0.06
+
+def pHeff_list(pC):
+    list = [pHsepa(pC), pHsepb(pC), pHcrois(pC)]
+    list.sort()
+    return list
+
+def pHeff1(pC):
+    return pHeff_list(pC)[0]
+def pHeff2(pC):
+    return pHeff_list(pC)[1]
+def pHeff3(pC):
+    return pHeff_list(pC)[2]
+
+def Esepza1(pC, pH):
     return Eza-0.06*pC
-def Esepab(pC, pH):
+def Esepza2(pC, pH):
+    return Esepza1(pC, pHsepa(pC)) - 0.06*(pH-pHsepa(pC))
+
+def Esepab1(pC, pH):
     return Eab
-def Esepzb(pC, pH):
-    return (Esepza(pC, pH)+Esepab(pC, pH))/2.
+def Esepab2(pC, pH):
+    return Esepab1(pC, pHsepa(pC)) + 0.06*(pH-pHsepa(pC))
+def Esepab3(pC, pH):
+    return Esepab2(pC, pHsepb(pC)) - 0.06*(pH-pHsepb(pC))
 
-def Ecible(pC, pH):
-    return Esepza(pC, pH) - 0.06*pH + 0.06*pHsepa(pC)
+def Esepzb1(pC, pH):
+    return (Esepza1(pC, pH)+Esepab1(pC, pH))/2.
+def Esepzb2(pC, pH):
+    return (Esepza2(pC, pH)+Esepab2(pC, pH))/2.
 
-def pHcible(pC):
-    return (Esepza(pC,0)-Esepab(pC, 0))/0.12+pHsepa(pC)
+def Esepzaeff(pC, pH):
+    if pH < pHsepa(pC):
+        return min([Esepza1(pC, pH), Esepzb1(pC, pH)])
+    else:
+        return min([Esepza2(pC, pH), Esepzb2(pC, pH)])
 
-# Not really
-sep1 = EpHsep('min', pHsepa, Esepza, Esepza)
-sep2 = EpHsep(pHsepa, 'max', Esepza, Ecible)
+def Esepabeff(pC, pH):
+    if pH <  pHsepa(pC):
+        return max([Esepab1(pC, pH), Esepzb1(pC, pH)])
+    elif pH < pHsepb(pC):
+        return max([Esepab2(pC, pH), Esepzb2(pC, pH)])
+    else:
+        return Esepab3(pC, pH)
+    
+sep1 = EpHsep('min', pHeff1, Esepzaeff, Esepzaeff)
+sep2 = EpHsep(pHeff1, pHeff2, Esepzaeff, Esepzaeff)
+sep3 = EpHsep(pHeff2, 'max', Esepzaeff, Esepzaeff)
+    
+sep4 = EpHsep('min', pHeff1, Esepabeff, Esepabeff)
+sep5 = EpHsep(pHeff1, pHeff2, Esepabeff, Esepabeff)
+sep6 = EpHsep(pHeff2, pHeff3, Esepabeff, Esepabeff)
+sep7 = EpHsep(pHeff3, 'max', Esepabeff, Esepabeff)
 
-def pHdeb(pC):
-    return min([pHsepa(pC),pHsepb(pC)])
-def pHfin(pC):
-    return max([pHsepa(pC),pHsepb(pC)])
+sep8 = EpHsep(pHsepb, pHsepb, Esepabeff, 'max')
 
-def Eciblea(pC, pH):
-    return Esepab(pC, pH) + 0.06*abs(pHsepb(pC) - pHsepa(pC))
-
-def Ecibleb(pC, pH):
-    return Eciblea(pC, pH) - 0.06*pH + 0.06*pHfin(pC)
-
-sep3 = EpHsep(pHsepb, pHsepb, 'max', Eciblea)
-sep4 = EpHsep(pHsepb, 'max', Eciblea, Ecibleb)
-sep5 = EpHsep('min', pHcible, Esepzb, Esepzb)
-sep6 = EpHsep(pHsepb, pHcible, Eciblea, Esepzb)
-sep7 = EpHsep(pHcible, 'max', Esepzb, Ecible)
+sep9 = EpHsep(pHsepa, pHsepa, Esepzaeff, Esepabeff)
 
 # if afficher_especes_chimiques is not False:
 #     ax.text(0.75*pH_min+0.25*pHsepb, Ecible+.1, 'Cu', color = text_diag_color)
@@ -52,5 +76,5 @@ sep7 = EpHsep(pHcible, 'max', Esepzb, Ecible)
 #     ax.text(0.25*pH_max+0.75*pHsepb, 0.75*Esepzb+0.25*E_max, 'Cu(OH)$_{2}$', color = text_diag_color)
 #     ax.text(0.85*pHsepb+0.15*pH_max, 0.85*Esepzb+0.15*Ecible-.025, 'Cu$_{2}$O', color = text_diag_color)
 
-seps = [sep3, sep4, sep5, sep6, sep7]
+seps = [sep1, sep2, sep3, sep4, sep5, sep6, sep7, sep8, sep9]
 spes = []
