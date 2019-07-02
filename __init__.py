f2e8f7cea896031823e2f900bb20295b28ad451f
lucastorterotot <lucas.torterotot@gmail.com>
2019-07-02 07:45:50 +0200

    init file for package improved, only relevant objects are imported (classes from core)

--- __init__.py
+++ __init__.py
@@ -1,1 +1,18 @@
-from ltLaTeXpyplot.module.core import *
+from ltLaTeXpyplot.module.core import (
+    ltFigure,
+    ltGraph,
+    ltPlotEpH,
+    ltPlotFct,
+    ltPlotFct3d,
+    ltPlotHist,
+    ltPlotNMR,
+    ltPlotPie,
+    ltPlotPieautopct,
+    ltPlotPts,
+    ltPlotPts3d,
+    ltPlotRegLin,
+    ltPlotScalField,
+    ltPlotSurf,
+    ltPlotVectField2d,
+    ltPlotVectField3d
+    )
