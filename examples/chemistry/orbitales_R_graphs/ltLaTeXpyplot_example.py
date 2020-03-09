d37285dc0c75e57d5ad8e683d8b7e911bdcc152a
lucastorterotot <lucas.torterotot@gmail.com>
2020-03-09 16:33:11 +0100

    fixup! Update on orbitals data

--- examples/chemistry/orbitales_R_graphs/ltLaTeXpyplot_example.py
+++ examples/chemistry/orbitales_R_graphs/ltLaTeXpyplot_example.py
@@ -10,23 +10,23 @@ r_on_a0 = np.arange(0,data.r_on_a0_MAX,0.1)
 
 radius = r_on_a0*data.a0
 
-to_process = {'30' : data.R_fcts['30'],
-              '21' : data.R_fcts['21']}
-for key, R_fct in to_process.items():
+Rnl = data.Rnl
+
+for n,l in [(3,0),(2,1)]:
     figs = {
-        'R' : lt.ltFigure(name='fig-R'+key, width_frac=.25),
-        'R2' : lt.ltFigure(name='fig-R'+key+'_squared', width_frac=.25),
-        'p' : lt.ltFigure(name='fig-R'+key+'_proba', width_frac=.25),
+        'R' : lt.ltFigure(name='fig-R{}{}'.format(n,l), width_frac=.25),
+        'R2' : lt.ltFigure(name='fig-R{}{}_squared'.format(n,l), width_frac=.25),
+        'p' : lt.ltFigure(name='fig-R{}{}_proba'.format(n,l), width_frac=.25),
         }
     x = r_on_a0
     ys = {
-        'R' : R_fct(radius),
-        'R2' : R_fct(radius)**2,
-        'p' : 4 * np.pi * (radius)**2 * R_fct(radius)**2,
+        'R' : Rnl(radius,n,l),
+        'R2' : Rnl(radius,n,l)**2,
+        'p' : 4 * np.pi * (radius)**2 * Rnl(radius,n,l)**2,
         }
     for subkey, fig in figs.items():
-        fig.addgraph('graph1', x_min=0, x_max=data.r_on_a0_max[key[0]+'x'], y_ticks=False, show_x_axis=True)
-        if subkey is not 'R' or key in ['10', '21', '32']:
+        fig.addgraph('graph1', x_min=0, x_max=data.r_on_a0_max['{}x'.format(n)], y_ticks=False, show_x_axis=True)
+        if subkey is not 'R' or n == l+1:
             fig.graphs['graph1'].y_min = 0
             fig.graphs['graph1'].y_max = 1
         fig.addplot(lt.ltPlotFct(x, ys[subkey]/max(ys[subkey])), 'graph1')
