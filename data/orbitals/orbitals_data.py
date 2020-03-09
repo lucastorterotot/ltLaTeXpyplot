782a6a99bd8450d06a17df43b35a60545dad47ca
lucastorterotot <lucas.torterotot@gmail.com>
2020-03-09 13:02:36 +0100

    Update on orbitals data

--- data/orbitals/orbitals_data.py
+++ data/orbitals/orbitals_data.py
@@ -1,57 +1,39 @@
 import numpy as np
-from scipy.constants import Planck, epsilon_0, electron_mass, elementary_charge
+from scipy.constants import Planck, epsilon_0, electron_mass, elementary_charge, proton_mass
 
 a0 = Planck*Planck * epsilon_0 / (np.pi * electron_mass * elementary_charge*elementary_charge)
-
-Z = 1 # for hydrogene
-alpha = Z/a0
 
 r_on_a0_max ={
     '1x' : 6,
     '2x' : 15,
-    '3x' : 30}
+    '3x' : 30,
+    '4x' : 50}
 r_on_a0_MAX = max([value for value in r_on_a0_max.values()])
 
-def R10(r):
-    u = Z*r/a0
-    return 2*alpha**(3./2)*np.exp(-u)
-def R20(r):
-    u = Z*r/a0
-    return alpha**(3./2)*2**-.5*(1-u/2)*np.exp(-u/2)
-def R21(r):
-    u = Z*r/a0
-    return alpha**(5./2)/(2*6**.5)*r*np.exp(-u/2)
-def R30(r):
-    u = Z*r/a0
-    return 2*alpha**(3./2)/(9*3**.5)*(3-2*u+2./9*u**2)*np.exp(-u/3)
-def R31(r):
-    u = Z*r/a0
-    return 4*alpha**(5./2)/(27*6**.5)*r*(2-u/3)*np.exp(-u/3)
-def R32(r):
-    u = Z*r/a0
-    return 4*alpha**(7./2)/(81*30**.5)*r**2*np.exp(-u/3)
-    
-R_fcts = {
-    '10' : R10,
-    '20' : R20,
-    '21' : R21,
-    '30' : R30,
-    '31' : R31,
-    '32' : R32
-}
+from scipy.special import genlaguerre
+from math import factorial
+
+def Rnl(r,n,l,Z=1,a0=a0):
+    u = Z/a0
+    return ((2* u/n)**3 * (factorial(n-l-1))/(2*n*(factorial(n+l))**3))**.5 * np.exp(-u/n*r) * (2* u/n * r)**l * genlaguerre(n-l-1,2*l+1)(2* u/n * r)
+
+# Shperical harmonics, see
+# https://en.wikipedia.org/wiki/Table_of_spherical_harmonics#Spherical_harmonics
 
 def Y00(theta, phi):
     return 1./(2*np.pi**.5)
+
 def Y10(theta, phi):
     return .5*(3./np.pi)**.5*np.cos(theta)
 def Y11p(theta, phi):
-    return .5*(3./(2*np.pi))**.5*np.sin(theta)*np.exp(phi*1j)
+    return -.5*(3./(2*np.pi))**.5*np.sin(theta)*np.exp(phi*1j)
 def Y11m(theta, phi):
     return .5*(3./(2*np.pi))**.5*np.sin(theta)*np.exp(-phi*1j)
+
 def Y20(theta, phi):
     return .25*(5./np.pi)**.5*(3*np.cos(theta)**2-1)
 def Y21p(theta, phi):
-    return .5*(15./(2*np.pi))**.5*np.sin(theta)*np.cos(theta)*np.exp(phi*1j)
+    return -.5*(15./(2*np.pi))**.5*np.sin(theta)*np.cos(theta)*np.exp(phi*1j)
 def Y21m(theta, phi):
     return .5*(15./(2*np.pi))**.5*np.sin(theta)*np.cos(theta)*np.exp(-phi*1j)
 def Y22p(theta, phi):
@@ -59,20 +41,56 @@ def Y22p(theta, phi):
 def Y22m(theta, phi):
     return .25*(15./(2*np.pi))**.5*np.sin(theta)**2*np.exp(-phi*2j)
 
+def Y30(theta, phi):
+    return .25 * (7/np.pi)**.5 * (5*np.cos(theta)**3 - 3*np.cos(theta))
+def Y31p(theta, phi):
+    return -1./8 * (21/np.pi)**.5 * np.sin(theta) * (5*np.cos(theta)**2-1) * np.exp(phi*1j)
+def Y31m(theta, phi):
+    return 1./8 * (21/np.pi)**.5 * np.sin(theta) * (5*np.cos(theta)**2-1) * np.exp(-phi*1j)
+def Y32p(theta, phi):
+    return 1./4 * (105/(2*np.pi))**.5 * np.sin(theta)**2 * np.cos(theta) * np.exp(phi*2j)
+def Y32m(theta, phi):
+    return 1./4 * (105/(2*np.pi))**.5 * np.sin(theta)**2 * np.cos(theta) * np.exp(-phi*2j)
+def Y33p(theta, phi):
+    return -1./8 * (35/np.pi)**.5 * np.sin(theta)**3 * np.exp(phi*3j)
+def Y33m(theta, phi):
+    return 1./8 * (35/np.pi)**.5 * np.sin(theta)**3 * np.exp(-phi*3j)
+
+def Y40(theta, phi):
+    return 3./16 * (1/np.pi)**.5 * (35 * np.cos(theta)**4 - 30 * np.cos(theta)**2 + 3)
+def Y41p(theta, phi):
+    return -3./8 * (5/np.pi)**.5 *np.sin(theta) * (7*np.cos(theta)**3-3*np.cos(theta)) * np.exp(phi*1j)
+def Y41m(theta, phi):
+    return 3./8 * (5/np.pi)**.5 *np.sin(theta) * (7*np.cos(theta)**3-3*np.cos(theta)) * np.exp(-phi*1j)
+def Y42p(theta, phi):
+    return 3./8 * (5/(2*np.pi))**.5 * np.sin(theta)**2 * (7*np.cos(theta)**2-1) * np.exp(phi*2j)
+def Y42m(theta, phi):
+    return 3./8 * (5/(2*np.pi))**.5 * np.sin(theta)**2 * (7*np.cos(theta)**2-1) * np.exp(-phi*2j)
+def Y43p(theta, phi):
+    return -3./8 * (35/np.pi)**.5 * np.sin(theta)**3 * np.cos(theta) * np.exp(phi*3j)
+def Y43m(theta, phi):
+    return 3./8 * (35/np.pi)**.5 * np.sin(theta)**3 * np.cos(theta) * np.exp(-phi*3j)
+def Y44p(theta, phi):
+    return 3./16 * (35/(2*np.pi))**.5 * np.sin(theta)**4 * np.exp(phi*4j)
+def Y44m(theta, phi):
+    return 3./16 * (35/(2*np.pi))**.5 * np.sin(theta)**4 * np.exp(-phi*4j)
+
 def Ys(theta, phi):
     return np.real(Y00(theta, phi))
+
 def Ypz(theta, phi):
     return np.real(Y10(theta, phi))
 def Ypx(theta, phi):
-    return np.real(.5**(.5) * (Y11p(theta, phi)+Y11m(theta, phi)))
+    return np.real(.5**(.5) * (-Y11p(theta, phi)+Y11m(theta, phi)))
 def Ypy(theta, phi):
-    return np.real(.5**(.5) / 1j * (Y11p(theta, phi)-Y11m(theta, phi)))
+    return np.real(.5**(.5) / 1j * (-Y11p(theta, phi)-Y11m(theta, phi)))
+
 def Ydz2(theta, phi):
     return np.real(Y20(theta, phi))
 def Ydxz(theta, phi):
-    return np.real(.5**(.5) * (Y21p(theta, phi)+Y21m(theta, phi)))
+    return np.real(.5**(.5) * (-Y21p(theta, phi)+Y21m(theta, phi)))
 def Ydyz(theta, phi):
-    return np.real(.5**(.5) / 1j * (Y21p(theta, phi)-Y21m(theta, phi)))
+    return np.real(.5**(.5) / 1j * (-Y21p(theta, phi)-Y21m(theta, phi)))
 def Ydx2my2(theta, phi):
     return np.real(.5**(.5) * (Y22p(theta, phi)+Y22m(theta, phi)))
 def Ydxy(theta, phi):
@@ -81,6 +99,22 @@ def Ydxy(theta, phi):
 #     return .25*(15./np.pi)**.5*(np.cos(theta)**2-np.sin(theta)**2*np.sin(phi)**2)
 # def Ydz2mx2(theta, phi):
 #     return .25*(15./np.pi)**.5*(np.cos(theta)**2-np.sin(theta)**2*np.cos(phi)**2)
+
+def Yfz3(theta, phi):
+    return np.real(Y30(theta, phi))
+def Yfxz2(theta, phi):
+    return np.real(.5**(.5) * (-Y31p(theta, phi)+Y31m(theta, phi)))
+def Yfyz2(theta, phi):
+    return np.real(.5**(.5) / 1j * (-Y31p(theta, phi)-Y31m(theta, phi)))
+def Yfxyz(theta, phi):
+    return np.real(.5**(.5) / 1j * (Y32p(theta, phi)-Y32m(theta, phi)))
+def Yfzx2my2(theta, phi):
+    return np.real(.5**(.5) * (Y32p(theta, phi)+Y32m(theta, phi)))
+def Yfxx2m3y2(theta, phi):
+    return np.real(.5**(.5) * (-Y33p(theta, phi)+Y33m(theta, phi)))
+def Yfy3x2my2(theta, phi):
+    return np.real(.5**(.5) / 1j * (-Y33p(theta, phi)-Y33m(theta, phi)))
+
 
 Y_fcts_C = {
     '00' : Y00,
@@ -91,7 +125,23 @@ Y_fcts_C = {
     '21p' : Y21p,
     '21m' : Y21m,
     '22p' : Y22p,
-    '22m' : Y22m
+    '22m' : Y22m,
+    '30' : Y30,
+    '31p' : Y31p,
+    '31m' : Y31m,
+    '32p' : Y32p,
+    '32m' : Y32m,
+    '33p' : Y33p,
+    '33m' : Y33m,
+    # '40' : Y40,
+    # '41p' : Y41p,
+    # '41m' : Y41m,
+    # '42p' : Y42p,
+    # '42m' : Y42m,
+    # '43p' : Y43p,
+    # '43m' : Y43m,
+    # '44p' : Y44p,
+    # '44m' : Y44m,
 }
 
 Y_fcts_R = {
@@ -103,9 +153,25 @@ Y_fcts_R = {
     'dxz' : Ydxz,
     'dyz' : Ydyz,
     'dxy' : Ydxy,
-    'dx2-y2' : Ydx2my2
+    'dx2-y2' : Ydx2my2,
+    'fz3' : Yfz3,
+    'fxz2' : Yfxz2,
+    'fyz2' : Yfyz2,
+    'fxyz' : Yfxyz,
+    'fz_x2-y2' : Yfzx2my2,
+    'fx_x2-3y2' : Yfxx2m3y2,
+    'fy3_x2-y2' : Yfy3x2my2,
 }
 
-orbitals = ['1s', '2s', '2px', '2py', '2pz', '3s', '3px', '3py', '3pz', '3dz2', '3dxz', '3dyz', '3dxy', '3dx2-y2']
+orbitals = ['1s',
+            '2s',
+            '2px', '2py', '2pz',
+            '3s',
+            '3px', '3py', '3pz',
+            '3dz2', '3dxz', '3dyz', '3dxy', '3dx2-y2',
+            '4s',
+            '4px', '4py', '4pz',
+            '4dz2', '4dxz', '4dyz', '4dxy', '4dx2-y2',
+            '4fz3', '4fxz2', '4fyz2', '4fxyz', '4fz_x2-y2', '4fx_x2-3y2', '4fy3_x2-y2']
 
 orbital_to_L = {'s':0, 'p':1, 'd':2, 'f':3}
