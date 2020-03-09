7398d5d231d47aa32bd01c131accc9badf623c9b
lucastorterotot <lucas.torterotot@gmail.com>
2020-03-09 16:47:51 +0100

    fixup! Update on orbitals data

--- examples/chemistry/orbitales_R_graphs/ltLaTeXpyplot_example-full.py
+++ examples/chemistry/orbitales_R_graphs/ltLaTeXpyplot_example-full.py
@@ -5,27 +5,32 @@ import numpy as np
 
 import ltLaTeXpyplot as lt
 import ltLaTeXpyplot.data.orbitals.orbitals_data as data # uses the orbital data of this package
+import matplotlib.pyplot as plt
 
 r_on_a0 = np.arange(0,data.r_on_a0_MAX,0.1)
 
 radius = r_on_a0*data.a0
 
-for key, R_fct in data.R_fcts.items():
-    figs = {
-        'R' : lt.ltFigure(name='fig-R'+key, width_frac=.25),
-        'R2' : lt.ltFigure(name='fig-R'+key+'_squared', width_frac=.25),
-        'p' : lt.ltFigure(name='fig-R'+key+'_proba', width_frac=.25),
+Rnl = data.Rnl
+
+for n in range(1,5):
+    for l in range(0,n):
+        figs = {
+            'R' : lt.ltFigure(name='fig-R{}{}'.format(n,l), width_frac=.25),
+            'R2' : lt.ltFigure(name='fig-R{}{}_squared'.format(n,l), width_frac=.25),
+            'p' : lt.ltFigure(name='fig-R{}{}_proba'.format(n,l), width_frac=.25),
         }
-    x = r_on_a0
-    ys = {
-        'R' : R_fct(radius),
-        'R2' : R_fct(radius)**2,
-        'p' : 4 * np.pi * (radius)**2 * R_fct(radius)**2,
+        x = r_on_a0
+        ys = {
+            'R' : Rnl(radius,n,l),
+            'R2' : Rnl(radius,n,l)**2,
+            'p' : 4 * np.pi * (radius)**2 * Rnl(radius,n,l)**2,
         }
-    for subkey, fig in figs.items():
-        fig.addgraph('graph1', x_min=0, x_max=data.r_on_a0_max[key[0]+'x'], y_ticks=False, show_x_axis=True)
-        if subkey is not 'R' or key in ['10', '21', '32']:
-            fig.graphs['graph1'].y_min = 0
-            fig.graphs['graph1'].y_max = 1
-        fig.addplot(lt.ltPlotFct(x, ys[subkey]/max(ys[subkey])), 'graph1')
-        fig.save(format='pdf')
+        for subkey, fig in figs.items():
+            fig.addgraph('graph1', x_min=0, x_max=data.r_on_a0_max['{}x'.format(n)], y_ticks=False, show_x_axis=True)
+            if subkey is not 'R' or n == l+1:
+                fig.graphs['graph1'].y_min = 0
+                fig.graphs['graph1'].y_max = 1
+            fig.addplot(lt.ltPlotFct(x, ys[subkey]/max(ys[subkey])), 'graph1')
+            fig.save(format='pdf')
+            plt.close()
