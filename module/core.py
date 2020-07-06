291f47b68cc9ec9920fb10c57dd70a1504611083
lucastorterotot <lucas.torterotot@gmail.com>
2020-07-06 11:25:13 +0200

    Add density plot for points

--- module/core.py
+++ module/core.py
@@ -773,7 +773,24 @@ class ltPlotPts(ltPlotFct):
             fig.graphs[graph].graph.errorbar(self.x, self.y, xerr=self.xerr, yerr=self.yerr, marker=self.marker, markersize=self.markersize, fmt=' ', linewidth=self.linewidth, elinewidth=self.elinewidth,capsize=self.capsize,capthick=self.capthick,color=self.color,label=self.label)
         else :
             fig.graphs[graph].graph.scatter(self.x, self.y, s=self.surface, c=self.color, marker=self.marker, cmap=self.cmap, alpha=self.alpha)
-        
+
+    def plot_density(self, fig, graph, bins = [100, 100]):
+        # Calculate the point density
+        from matplotlib.colors import Normalize
+        from scipy.interpolate import interpn
+
+        data , x_e, y_e = np.histogram2d(self.x, self.y, bins, density = True)
+        z = interpn(
+            ( 0.5*(x_e[1:] + x_e[:-1]) , 0.5*(y_e[1:]+y_e[:-1]) ),
+            data,
+            np.vstack([self.x, self.y]).T,
+            method = "splinef2d",
+            bounds_error = False
+        )
+        z[np.where(np.isnan(z))] = 0.0
+        idx = z.argsort()
+        x, y, z = self.x[idx], self.y[idx], z[idx]
+        fig.graphs[graph].graph.scatter(x, y, c=z, marker=self.marker, edgecolor='', label=self.label, cmap=self.cmap, alpha=self.alpha)
 
 class ltPlotPts3d(ltPlotPts):
     def __init__(self, x, y, z, label=None, color=color_default, marker=marker_pts_default, markersize=marker_size_default, cmap=None, norm_xy=True, norm_xyz=False, surface=None, alpha=None):
