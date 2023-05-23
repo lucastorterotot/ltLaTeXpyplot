51e1a668860d53c8c69e7b0ac47f2a8d257e0e41
Lucas Torterotot <lucas.torterotot@gmail.com>
2023-05-23 08:33:18 +0200

    Update I data and tests

--- data/EpH/I.py
+++ data/EpH/I.py
@@ -4,30 +4,24 @@ Eaz = 0.54
 Ezb = 1.19
 
 def Esepaz(pC, pH):
-    c_tr = 10**(-pC)
-    return Eaz + RT_on_F/2* np.log10(2./c_tr)
+    return Eaz + RT_on_F*pC
 
 def pHsep(pC):
-    c_tr = 10**(-pC)
-    return 10./(RT_on_F*12)*(Ezb - Esepaz(pC,0) +RT_on_F/10*np.log10(c_tr/2.))
+    return (Ezb - Eaz - RT_on_F*pC*(1+1/5))/RT_on_F*10/12
 
-def Esepzb1(pC, pH):
-    c_tr = 10**(-pC)
-    return Ezb + RT_on_F/10*np.log10(c_tr/2.) - RT_on_F*12./10.*pH
+def Esepzb(pC, pH):
+    return Ezb - RT_on_F/5*pC - RT_on_F*12/10*pH
 
-def Esepzb2(pC, pH):
-    return Esepaz(pC, pH)
-
-def Esepab2(pC, pH):
-    return Esepzb2(pC, pH) - RT_on_F*(pH - pHsep(pC))
+def Esepab(pC, pH):
+    return Esepzb(pC, pHsep(pC)) - RT_on_F*(pH - pHsep(pC))
 
 sep1 = EpHsep('min', pHsep, Esepaz, Esepaz)
-sep2 = EpHsep(pHsep, 'max', Esepaz, Esepab2)
-sep3 = EpHsep('min', pHsep, Esepzb1, Esepzb2)
+sep2 = EpHsep(pHsep, 'max', Esepab, Esepab)
+sep3 = EpHsep('min', pHsep, Esepzb, Esepzb)
 
-spe1 = EpHspe('I$_2$', 'min', pHsep, Esepzb1, Esepaz, pH_r=.25, E_r=.6)
-spe2 = EpHspe('IO$_3^-$', 'min', 'max', 'max', Esepzb1, pH_r=.75, E_r=.25)
-spe3 = EpHspe('I$^-$', 'min', 'max', Esepaz, Esepab2)
+spe1 = EpHspe('I$_2$', 'min', pHsep, Esepzb, Esepaz, pH_r=.25, E_r=.6)
+spe2 = EpHspe('IO$_3^-$', 'min', 'max', 'max', Esepzb, pH_r=.75, E_r=.25)
+spe3 = EpHspe('I$^-$', 'min', 'max', Esepaz, Esepab)
 
 seps = [sep1, sep2, sep3]
 spes = [spe1, spe2, spe3]
